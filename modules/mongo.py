f8a872bbf7627f15cb6a4666c22227c4d7e86149
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-06-12 17:12:54 +0800

    fix a stupid bug

--- modules/mongo.py
+++ modules/mongo.py
@@ -1,14 +1,15 @@
 from typing import Dict, Any, Optional
-
+from pymongo.results import UpdateResult
 from pymongo.asynchronous.collection import AsyncCollection
 
 
-async def update_data(user_id: int, data: Dict[str, Any], collection: AsyncCollection) -> None:
-    await collection.update_one(
+async def update_data(user_id: int, data: Dict[str, Any], collection: AsyncCollection) -> UpdateResult:
+    result = await collection.update_one(
         filter={"_id": user_id},
         update={"$set": data},
         upsert=True,
     )
+    return result
 
 
 async def get_data(user_id: int, server: str, collection: AsyncCollection) -> Optional[Dict[str, Any]]:
