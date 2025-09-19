fecba087a8b1432dada654c43ad9c36bde4d87b9
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-09-20 02:47:52 +0800

    Migrate sekaiclient: cryptor and const

---
+++ utils/sekaiclientdummy/const.go
@@ -0,0 +1,25 @@
+package sekaiclientdummy
+
+const General = "/6O9YhTzP+c8ty/uImK+2w=="
+
+var Refresh = map[string]interface{}{
+	"refreshableTypes": []string{
+		"new_pending_friend_request",
+		"user_report_thanks_message",
+		"streaming_virtual_live_reward_status",
+	},
+}
+var RefreshLogin = map[string]interface{}{
+	"refreshableTypes": []string{
+		"new_pending_friend_request",
+		"login_bonus",
+		"user_report_thanks_message",
+		"streaming_virtual_live_reward_status",
+	},
+}
+var MySekaiRoom = map[string]interface{}{
+	"roomProperty": map[string]interface{}{
+		"isRSend": 1,
+		"values":  map[string]interface{}{},
+	},
+}
--- utils/sekaiclientdummy/cryptor.go
+++ utils/sekaiclientdummy/cryptor.go
@@ -1,1 +1,73 @@
 package sekaiclientdummy
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"haruki-suite/utils"
+
+	"github.com/vgorin/cryptogo/pad"
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+var (
+	generalAESKey, _ = hex.DecodeString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	generalAESIV, _  = hex.DecodeString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+
+	enAESKey, _ = hex.DecodeString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+	enAESIV, _  = hex.DecodeString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+)
+
+func getCipher(server utils.SupportedDataUploadServer, encrypt bool) (cipher.BlockMode, error) {
+	var key, iv []byte
+	if server == utils.SupportedDataUploadServerEN {
+		key, iv = enAESKey, enAESIV
+	} else {
+		key, iv = generalAESKey, generalAESIV
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if encrypt {
+		return cipher.NewCBCEncrypter(block, iv), nil
+	}
+	return cipher.NewCBCDecrypter(block, iv), nil
+}
+
+func Pack(content interface{}, server utils.SupportedDataUploadServer) ([]byte, error) {
+	packed, err := msgpack.Marshal(content)
+	if err != nil {
+		return nil, err
+	}
+
+	padded := pad.PKCS7Pad(packed, aes.BlockSize)
+
+	encrypter, err := getCipher(server, true)
+	if err != nil {
+		return nil, err
+	}
+
+	encrypted := make([]byte, len(padded))
+	encrypter.CryptBlocks(encrypted, padded)
+	return encrypted, nil
+}
+
+func Unpack(content []byte, server utils.SupportedDataUploadServer, out interface{}) error {
+	decrypter, err := getCipher(server, false)
+	if err != nil {
+		return err
+	}
+
+	decrypted := make([]byte, len(content))
+	decrypter.CryptBlocks(decrypted, content)
+
+	unpadded, err := pad.PKCS7Unpad(decrypted)
+	if err != nil {
+		return err
+	}
+
+	return msgpack.Unmarshal(unpadded, out)
+}
