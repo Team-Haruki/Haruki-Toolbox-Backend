9b71377c36e8a5e7a36a753b90fe790461361d0c
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-09-21 23:18:22 +0800

    Patch from MiddleRed

--- api/helper.go
+++ api/helper.go
@@ -3,6 +3,9 @@ package api
 import (
 	Utils "haruki-suite/utils"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,4 +20,38 @@ func JSONResponse(c *fiber.Ctx, resp Utils.APIResponse) error {
 
 func IntPtr(v int) *int {
 	return &v
+}
+
+func ExtractUploadTypeAndUserID(originalURL string) (Utils.UploadDataType, int64) {
+	if strings.Contains(originalURL, string(Utils.UploadDataTypeSuite)) {
+		re := regexp.MustCompile(`user/(\d+)`)
+		match := re.FindStringSubmatch(originalURL)
+
+		if len(match) < 2 {
+			return "", 0
+		}
+
+		userID, err := strconv.ParseInt(match[1], 10, 64)
+		if err != nil {
+			return "", 0
+		}
+
+		return Utils.UploadDataTypeSuite, userID
+
+	} else if strings.Contains(originalURL, string(Utils.UploadDataTypeMysekai)) {
+		re := regexp.MustCompile(`user/(\d+)`)
+		match := re.FindStringSubmatch(originalURL)
+
+		if len(match) < 2 {
+			return "", 0
+		}
+
+		userID, err := strconv.ParseInt(match[1], 10, 64)
+		if err != nil {
+			return "", 0
+		}
+		return Utils.UploadDataTypeMysekai, userID
+	}
+
+	return "", 0
 }
