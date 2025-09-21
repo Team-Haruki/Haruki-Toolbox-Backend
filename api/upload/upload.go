4c05d089c1c264e430c01138a78b793bb6fb6366
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-09-21 23:18:09 +0800

    Patch from MiddleRed

--- api/upload/upload.go
+++ api/upload/upload.go
@@ -1,1 +1,12 @@
 package upload
+
+import (
+	harukiMongo "haruki-suite/utils/mongo"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterRoutes(app *fiber.App, manager *harukiMongo.MongoDBManager, secret string) {
+	registerIosRoutes(app, manager, secret)
+	registerGeneralRoutes(app, manager, secret)
+}
