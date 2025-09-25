ab582a5b9b5102770ee6432e30dcbb14ccdbcc72
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-09-25 15:13:07 +0800

    Fix resty client body error

--- utils/http/client.go
+++ utils/http/client.go
@@ -39,20 +39,24 @@ func (c *Client) init() error {
 }
 
 func (c *Client) Request(ctx context.Context, method, uri string, headers map[string]string, body []byte) (int, map[string]string, []byte, error) {
-	resp, err := c.client.R().
-		SetContext(ctx).
-		SetHeaders(headers).
-		SetBody(body).
-		Execute(method, uri)
-	if err != nil {
-		return 0, nil, nil, err
-	}
+    req := c.client.R().
+        SetContext(ctx).
+        SetHeaders(headers)
 
-	respHeaders := make(map[string]string, len(resp.Header()))
-	for k, v := range resp.Header() {
-		if len(v) > 0 {
-			respHeaders[k] = v[0]
-		}
-	}
-	return resp.StatusCode(), respHeaders, resp.Body(), nil
+    if body != nil && len(body) > 0 {
+        req.SetBody(body)
+    }
+
+    resp, err := req.Execute(method, uri)
+    if err != nil {
+        return 0, nil, nil, err
+    }
+
+    respHeaders := make(map[string]string, len(resp.Header()))
+    for k, v := range resp.Header() {
+        if len(v) > 0 {
+            respHeaders[k] = v[0]
+        }
+    }
+    return resp.StatusCode(), respHeaders, resp.Body(), nil
 }
