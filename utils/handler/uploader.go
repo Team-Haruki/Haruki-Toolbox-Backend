d64956ac06c3fda5d634f121fb2dc972f7c57fa3
MejiroRina <70424266+MejiroRina@users.noreply.github.com>
2025-10-08 08:12:47 +0800

    [Feat] support data sync to 8823bot, add sync settings api

--- utils/handler/uploader.go
+++ utils/handler/uploader.go
@@ -7,6 +7,7 @@ import (
 	apiHelper "haruki-suite/utils/api"
 	harukiLogger "haruki-suite/utils/logger"
 	harukiVersion "haruki-suite/version"
+	"strconv"
 	"strings"
 
 	"github.com/go-resty/resty/v2"
@@ -22,8 +23,33 @@ func DataUploader(url string, userID int64, server utils.SupportedDataUploadServ
 		httpClient := resty.New()
 		resp, err := httpClient.R().
 			SetHeader("User-Agent", fmt.Sprintf("Haruki-Toolbox-Backend/%s", harukiVersion.Version)).
-			SetHeader("Accept", "application/json").
+			SetHeader("Accept", "application/octet-stream").
 			SetHeader("Authorization", fmt.Sprintf("Bearer %s", endpointSecret)).
+			SetBody(rawData).
+			Post(url)
+		if err != nil {
+			logger.Warnf("Failed to sync data to %s: %v", url, err)
+		}
+		if resp.StatusCode() != 200 {
+			logger.Warnf("Failed to sync data to %s: status code %v", url, resp.Status())
+		} else {
+			logger.Infof("Successfully sync data to %s", url)
+		}
+	} else {
+		logger.Warnf("Upload endpoint url is empty, skipped syncing data.")
+	}
+}
+
+func Sync8823(url string, userID int64, server utils.SupportedDataUploadServer, dataType utils.UploadDataType, rawData []byte, endpointSecret string) {
+	if url != "" {
+		httpClient := resty.New()
+		resp, err := httpClient.R().
+			SetHeader("User-Agent", fmt.Sprintf("Haruki-Toolbox-Backend/%s", harukiVersion.Version)).
+			SetHeader("Accept", "application/octet-stream").
+			SetHeader("X-Credentials", endpointSecret).
+			SetHeader("X-Server-Region", string(server)).
+			SetHeader("X-Upload-Type", string(dataType)).
+			SetHeader("X-User-Id", strconv.FormatInt(userID, 10)).
 			SetBody(rawData).
 			Post(url)
 		if err != nil {
@@ -43,7 +69,7 @@ func DataSyncer(userID int64, server utils.SupportedDataUploadServer, dataType u
 	if dataType == utils.UploadDataTypeSuite {
 		if settings.Suite != nil {
 			if settings.Suite.Allow8823 {
-				go DataUploader(harukiConfig.Cfg.ThirdPartyDataProvider.Endpoint8823, userID, server, dataType, rawData, harukiConfig.Cfg.ThirdPartyDataProvider.Secret8823)
+				go Sync8823(harukiConfig.Cfg.ThirdPartyDataProvider.Endpoint8823, userID, server, dataType, rawData, harukiConfig.Cfg.ThirdPartyDataProvider.Secret8823)
 				logger.Infof("Syncing suite data to 8823...")
 			}
 			if settings.Suite.AllowSakura {
@@ -63,7 +89,7 @@ func DataSyncer(userID int64, server utils.SupportedDataUploadServer, dataType u
 	if dataType == utils.UploadDataTypeMysekai {
 		if settings.Mysekai != nil {
 			if settings.Mysekai.Allow8823 {
-				go DataUploader(harukiConfig.Cfg.ThirdPartyDataProvider.Endpoint8823, userID, server, dataType, rawData, harukiConfig.Cfg.ThirdPartyDataProvider.Secret8823)
+				go Sync8823(harukiConfig.Cfg.ThirdPartyDataProvider.Endpoint8823, userID, server, dataType, rawData, harukiConfig.Cfg.ThirdPartyDataProvider.Secret8823)
 				logger.Infof("Syncing mysekai data to 8823...")
 			}
 			if settings.Mysekai.AllowResona {
